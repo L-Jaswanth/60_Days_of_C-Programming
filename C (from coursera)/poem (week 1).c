35b2239acb0a656750b6e62f20c8e907aacc69e1
L-Jaswanth <jaswanthlsvyj@gmail.com>
2022-05-20 22:44:03 +0530

    first commit

---
+++ C (from coursera)/poem (week 1).c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+     printf("\nMary had a little lamb,");
+     printf("\nLittle lamb, little lamb,");
+     printf("\nMary had a little lamb,");
+     printf("\nIts fleece was white as snow,");
+     printf("\nAnd every where that Mary went,");
+     printf("\nMary went, Mary went,");
+     printf("\nEverywhere that Mary went,");
+     printf("\nThe lamb was sure to go");
+     printf("\nHe followed her to school one day,");
+     printf("\nSchool one day, school one day,");
+     printf("\nHe followed her to school one day,");
+     printf("\nWhich was against the rule;");
+     printf("\nWhichIt made the children laugh and play,");
+     printf("\nLaugh and play, laugh and play,");
+     printf("\nIt made the children laugh and play,");
+     printf("\nTo see a lamb at school,");
+     printf("\nAnd so the teacher turned him out,");
+     printf("\nTurned him out, turned him out,");
+     printf("\nSo the teacher turned him out,");
+     printf("\nBut still he lingered near,");
+     printf("\nAnd waited patiently about,");
+     printf("\nPatiently about, patiently about,");
+     printf("\nWaited patiently about,");
+     printf("\nTill Mary did appear;");
+     printf("\n“Why does the lamb love Mary so?");
+     printf("\nMary so, Mary so,");
+     printf("\nWhy does the lamb love Mary so?”");
+     printf("\nThe eager children cried;");
+     printf("\n“Why Mary loves the lamb, you know,");
+     printf("\nLamb you know, lamb you know,");
+     printf("\nWhy Mary loves the lamb, you know”");
+     printf("\nThe teacher did reply;");
+     printf("\nMary had a little lamb,");
+     printf("\nLittle lamb, little lamb,");
+     printf("\nMary had a little lamb,");
+     printf("\nIts fleece was white as snow.");
+
+    return 0;
+}
